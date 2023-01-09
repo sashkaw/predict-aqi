24b72e0eec7053034d2532f04e7f49b9604365f3
duraznoj <sashka.warner@gmail.com>
2023-01-09 02:33:41 -0800

    Add data transformation functions

--- backend/apps.py
+++ backend/apps.py
@@ -7,7 +7,9 @@ from django.conf import settings
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend'
-    # Load ML model once when app starts up
+    # Load ML model and data scaler once when app starts up
     #TODO: update this with new model
     #MODEL_FILE = os.path.join(settings.MODELS, "aqi_LSTM.joblib")
+    #SCALER_FILE = os.path.join(settings.MODELS, "aqi_scaler.joblib")
     #model = joblib.load(MODEL_FILE)
+    #scaler = joblib.load(SCALER_FILE)
--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,9 @@
 import numpy as np
 import pandas as pd
 import requests
-from datetime import datetime
+from time import mktime
+from datetime import datetime, timedelta
+from sklearn.preprocessing import MinMaxScaler
 from .apps import *
 from django.conf import settings
 from django.shortcuts import render
@@ -10,8 +12,89 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 # Constants
-#WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/air_pollution/history'
-WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/air_pollution'
+
+WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/air_pollution/history'
+#WEATHER_API_URL = 'http://api.openweathermap.org/data/2.5/air_pollution'
+
+# Helper functions
+
+# Transform time series to stationary:
+# Create a differenced series to remove any increasing trend
+def difference(dataset, interval=1):
+    diff = [None] * interval # Start with null value(s) so length matches the other data frames
+    # Iterate through 1 - n records
+    for i in range(interval, len(dataset)):
+        # Calculate difference between current and timestep and past timestep
+        value = dataset[i] - dataset[i - interval]
+        diff.append(value)
+
+    return pd.Series(diff)
+
+# Scale data to [-1, 1]
+def scale(data, scaler):
+    # transform data (possibly not needed for 1d timeseries)
+    data = data.reshape(data.shape[0], data.shape[1])
+    data_scaled = scaler.transform(data)
+
+    return data_scaled
+ 
+# inverse scaling for a forecasted value
+def invert_scale(scaler, yhat):
+    array = np.array(yhat)
+    array = array.reshape(1, len(array))
+    inverted = scaler.inverse_transform(array)
+    return inverted[0, 0]
+
+def forecast_aqi(X):
+    '''
+    Transform input air quality time series data and forecast future air quality.
+
+    Parameters:
+    X --- list of numeric air quality time series data
+
+    Return:
+    forecast --- forecasted air quality in numeric format
+    '''
+
+    # Difference data to remove trends
+    X_diff = difference(X)
+
+    # Define batch size for model prediction
+    batch_size = 4
+
+    # Get data scaler that was loaded on app start
+    # This scaler will transform the data between -1 and 1
+    #scaler = BackendConfig.scaler
+
+    # Scale the differenced data
+    #aqi_scaled = scale(X_diff[1])
+    
+    # Get LSTM model that was loaded on app start
+    #model = BackendConfig.model
+    # Fit and evaluate model
+    #X_trimmed = aqi_scaled[1]
+
+    # Reshape
+    #reshaped = X_trimmed.reshape(len(X_trimmed), 1, 1)
+    
+    # Forecast dataset
+    #output = model.predict(reshaped, batch_size=batch_size)
+
+    # Invert data transforms on forecast
+    #yhat = output[0]
+
+    # Invert scaling
+    #yhat = invert_scale(scaler, X[1], yhat)
+
+    # invert differencing
+    #yhat = yhat + X[1]
+
+    # store forecast
+    #forecast = yhat
+
+    #return forecast
+    return X_diff[1]
+    
 
 # Create your views here.
 class Prediction(APIView):
@@ -23,31 +106,47 @@ class Prediction(APIView):
         Make request to external API to get current air quality data.
 
         Return:
-        1) JSON formatted response from external API on success, OR
+        1) time_series --- list of numeric air quality time series data
         2) -1 on failure
         '''
         # Create url from params
 
-        # Specify position
+        # Specify position for API query
         lat = 44.026280
         long = -123.083715
 
-        # Get current time
-        #date2 = datetime.utcnow()
-        #date1 = datetime.utcnow() - 1000
+        # Get the most recent hour, on the dot
+        d2 = datetime.now().replace(microsecond=0, second=0, minute=0)
+        one_hr = timedelta(hours=1)
+        # Get one hour before the most recent hour
+        d1 = d2 - one_hr
+        # Convert to unix time for API call
+        d1_unix = int(mktime(d1.timetuple()))
+        d2_unix = int(mktime(d2.timetuple()))
 
+        # Specify query parameters for API call
         params = {
             'lat': lat,
             'lon': long,
-            #'start': date1,
-            #'end': date2,
+            'start': d1_unix,
+            'end': d2_unix,
             'appid': settings.WEATHER_API_KEY,
         }
 
         # Fetch data from external API
         try:
             api_response = requests.get(url=WEATHER_API_URL, params=params)
-            return api_response.json()
+            api_json = api_response.json()
+            # Extract air quality data
+            api_data = list(api_json.get('list', {}))
+            time_series = list()
+            # Extract air quality data of interest
+            for item in api_data:
+                current_val = item.get('components', {}).get('pm2_5', None)
+                time_series.append(current_val)
+
+            #print(time_series)
+            return time_series
         except:
             return -1
         
@@ -66,25 +165,21 @@ class Prediction(APIView):
 
         if(current_aqi != -1):
         
-            print(current_aqi)
+            #print(current_aqi)
+            forecast = forecast_aqi(current_aqi)
 
-            # TODO:
-            # Transform data: 
-            # 1) Difference from previous value
-            # 2) Normalize
-            
-            # Get LSTM model that was loaded on app start
-            #lstm_model = BackendConfig.model
-
-            # TODO:
-            # Transform data for output:
-            # 1) Reverse normalization
-            # 2) Add to previous value to reverse differencing
+            # Difference data to remove trend, 
+            # scale between [-1, 1],
+            # make prediction for next air quality value
+            # using the LSTM model,
+            # invert the scaling and differencing of the output
+            # to match the input data format
+            #forecast_aqi = forecast_aqi(current_aqi)
             
             # Make prediction based on current air quality
             #prediction = lstm_model.predict(np.array([[current_aqi]]))
             context = {
-                'data': current_aqi, # For testing
+                'data': forecast,
                 }
 
             return Response(context, status=status.HTTP_200_OK)
