b9140e0d25c987f1e909bee6a1393907bf953a34
duraznoj <sashka.warner@gmail.com>
2023-01-19 16:12:56 -0800

    Update allowed hosts

--- aqi_forecast/settings.py
+++ aqi_forecast/settings.py
@@ -32,7 +32,7 @@ WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['predict-aqi.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
