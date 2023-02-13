9cfedea92c80a4059179e901c64856071ed8ed0a
duraznoj <sashka.warner@gmail.com>
2023-02-12 22:04:25 -0800

    Update getenv

--- aqi_forecast/settings.py
+++ aqi_forecast/settings.py
@@ -60,7 +60,7 @@ elif os.getenv("TRAMPOLINE_CI", None):
     )
     env.read_env(io.StringIO(placeholder))
 # [END_EXCLUDE]
-elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+'''elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     # Pull secrets from Secret Manager
     project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
 
@@ -70,9 +70,17 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
-    env.read_env(io.StringIO(payload))
-else:
-    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+    env.read_env(io.StringIO(payload))'''
+#else:
+#    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# Pull secrets from Secret Manager
+project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+client = secretmanager.SecretManagerServiceClient()
+***REMOVED***
+***REMOVED***
+name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 # [END gaestd_py_django_secret_config]
 
 SECRET_KEY = env("SECRET_KEY")
